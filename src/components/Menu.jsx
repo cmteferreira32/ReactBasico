173958726ce73f440e55e69666498b1e150d52c6
João Paulo Araújo Ferreira <cmteferreira32@hotmail.com>
2022-03-23 22:17:13 -0300

    Ultima versão - Atualizada

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,16 +1,20 @@
 import React, { useCallback } from 'react'
+import { Container, Nav, Navbar} from "react-bootstrap";
 
 const Menu = () => {
   return (
-    <div>
-<ul>
-    <li><a href="">Página 1</a></li>
-    <li><a href="">Página 2</a></li>
-    <li><a href="">Página 3</a></li>
-</ul>
 
+<Navbar bg="primary" variant="dark" className="mb-2">
+    <Container>
+    <Navbar.Brand href="#home"><b>Front-end</b></Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Components</Nav.Link>
+      <Nav.Link href="#features">Guides</Nav.Link>
+      <Nav.Link href="#pricing">API</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
 
-    </div>
   )
 }
 export default Menu
